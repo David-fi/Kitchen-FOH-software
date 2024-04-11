ff22591bc66d14643366f008a2a162602ef8fdd3
bryankhoo07 <bryan.khoo@city.ac.uk>
2024-04-11 01:21:49 +0100

    Database connection

---
+++ src/main/java/com/example/javateamproject/DatabaseInteractions.java
@@ -0,0 +1,48 @@
+package com.example.javateamproject;
+
+import java.sql.*;
+
+public class DatabaseInteractions {
+
+    private Connection con;
+    private Statement st;
+
+    public DatabaseInteractions() {
+        createConnection();
+    }
+
+    public int addIngredient(String ingredientName) {
+        String insertString = "INSERT INTO Ingredient VALUES (?)";
+        try (PreparedStatement insertStmt = this.con.prepareStatement(insertString)) {
+            insertStmt.setString(1, ingredientName);
+            int affectedRows = insertStmt.executeUpdate();
+            if (affectedRows == 0) {
+                throw new SQLException("Creating ingredient failed, no rows affected.");
+            }
+
+            try (ResultSet generatedKeys = insertStmt.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    return generatedKeys.getInt(1);
+                } else {
+                    throw new SQLException("Creating ingredient failed, no ID obtained.");
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Create Ingredient Error: " + e.getMessage());
+        }
+        return -1;
+    }
+
+    public void createConnection() {
+        try {
+            String url = "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2033t35";
+            String user = "in2033t35_d";
+            String password = "pSRHdoIqm74";
+            this.con = DriverManager.getConnection(url, user, password);
+            this.st = con.createStatement();
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+}
+
+    }
+}
--- src/main/java/com/example/javateamproject/KitchenApplication.java
+++ src/main/java/com/example/javateamproject/KitchenApplication.java
@@ -1,5 +1,5 @@
 package com.example.javateamproject;
-
+    
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
