842a533c407e49274ef4985f80bb9456acd22966
bryankhoo07 <bryan.khoo@city.ac.uk>
2024-04-14 19:46:04 +0100

    Updates on remove edit and remove on recipe and dish class(QUERIES)

--- src/main/java/com/example/javateamproject/HomeController.java
+++ src/main/java/com/example/javateamproject/HomeController.java
@@ -9,8 +9,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Modality;
@@ -29,16 +27,6 @@ public class HomeController  {
     private Label usernameLabel;
     @FXML
     private ImageView signinImage;
-    @FXML
-    private TextArea CreateDishEnterDishID;
-    @FXML
-    private TextField CreateDishEnterDishName;
-    @FXML
-    private TextArea CreateDishEnterRecipeID;
-    @FXML
-    private TextField CreateDishEnterPhotoPath;
-    @FXML
-    private TextField CreateDishEnterPreparationTime;
 
 
     public void initialize() {
